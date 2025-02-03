79d049c0d1749a15e7762b4433137f12d61abcc4
Nikki <nikkizhang320@gmail.com>
2025-02-02 22:51:17 -0500

    Added methods and variables to classes

---
+++ src/Hours.java
@@ -0,0 +1,13 @@
+public class Hours {
+    private int hours;
+
+    public Hours() { }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void addHours(int hoursAdded) {
+        hours += hoursAdded;
+    }
+}
--- src/Organization.java
+++ src/Organization.java
@@ -1,2 +1,33 @@
+import java.util.Scanner;
+
 public class Organization {
+    private boolean PlaytimeForCharity;
+    private boolean ServiceSquad;
+    private boolean MealsOnWhales;
+    private String current;
+    private int hours;
+    private Scanner scanner;
+
+    public Organization (String organization) {
+        current = organization;
+        hours = 0;
+        scanner = new Scanner(System.in);
+    }
+
+    public void lookForVolunteer() {
+        double chance = Math.random();
+        if (chance >= 0.4) {
+            System.out.println("You found an opportunity for volunteering! ");
+            if (current.equals("PTC")) {
+                // different things to volunteer for this organization
+            } else if (current.equals("SS")) {
+                // different things to volunteer for this organization
+            } else {
+                // different things to volunteer for this organization
+            }
+        } else {
+            System.out.println("The organization is currently not holding any events.");
+            System.out.println("Try again or try looking for another organization!");
+        }
+    }
 }
